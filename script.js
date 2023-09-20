599199facf29124a0ae63278293cbc5c27b7efb6
mudasirCs <mudasir.cs47@gmail.com>
2023-09-20 13:22:55 +0500

    Complete version 1.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gameBoard = (() => {
   let _gameBoard;
-  const _gameBoardInit = () => {
+  const gameBoardInit = () => {
     _gameBoard = Array.from({ length: 3 }, () => Array(3).fill(""));
   };
   const fillBoardPosition = (x, y, mark) => {
@@ -8,80 +8,100 @@ const gameBoard = (() => {
   };
   const getBoardPosition = (x, y) => _gameBoard[x][y];
   //   const checkBoardPosition = (x, y, mark) => (gameBoard[x][y] = mark ? 1 : 0);
+  const isBoardBlank = () =>
+    _gameBoard.every((row) => row.every((cell) => cell === ""));
   const checkBoardEmpty = (x, y) => (!_gameBoard[x][y] ? true : false);
   const checkBoardWin = () => {
-    //add genrator iterable logic in future
-    /**
-       X 0 X
-      0 X 0
-      0 0 X 
-
-      1 1 1
-      2 2 2
-      2 2 3
-      */
-    //check rows
-    let mark = "";
+    let mark;
     let streak = 0;
-    let win = 0;
+
+    // Check rows and columns
     for (let i = 0; i < 3; i++) {
       mark = _gameBoard[i][0];
-      streak = _gameBoard[i].reduce((streak, val) => {
-        return val == mark ? ++streak : 0;
-      }, 0);
-      if (streak == 3) return { win: 1, mark };
-    }
-    //check columns
-    for (let i = 0; i < 3; i++) {
-      mark = _gameBoard[0][i];
-      streak = _gameBoard.reduce((streak, val) => {
-        return val[i] == mark ? ++streak : 0;
-      }, 0);
-      if (streak == 3) return { win: 1, mark };
-    }
-    //check diagonal
-    mark = _gameBoard[0][0];
-    streak = _gameBoard.reduce((streak, val, index) => {
-      return val[index] == mark ? ++streak : 0;
-    }, 0);
-    if (streak == 3) return { win: 1, mark };
-    //check anti-diagonal
-    mark = _gameBoard[0][2];
-    streak = _gameBoard.reduce((streak, val, index) => {
-      return val[2 - index] == mark ? ++streak : 0;
-    }, 0);
-    if (streak == 3) return { win: 1, mark };
+      if (mark !== "") {
+        streak = _gameBoard[i].reduce(
+          (streak, val) => (val === mark ? streak + 1 : 0),
+          0
+        );
+        if (streak === 3) return { win: 1, mark };
+      }
 
-    return { win: 0, mark };
+      mark = _gameBoard[0][i];
+      if (mark !== "") {
+        streak = _gameBoard.reduce(
+          (streak, val) => (val[i] === mark ? streak + 1 : 0),
+          0
+        );
+        if (streak === 3) return { win: 1, mark };
+      }
+    }
+
+    // Check diagonal and anti-diagonal
+    mark = _gameBoard[0][0];
+    if (mark !== "") {
+      streak = _gameBoard.reduce(
+        (streak, val, index) => (val[index] === mark ? streak + 1 : 0),
+        0
+      );
+      if (streak === 3) return { win: 1, mark };
+    }
+
+    mark = _gameBoard[0][2];
+    if (mark !== "") {
+      streak = _gameBoard.reduce(
+        (streak, val, index) => (val[2 - index] === mark ? streak + 1 : 0),
+        0
+      );
+      if (streak === 3) return { win: 1, mark };
+    }
+
+    return { win: 0, mark: "" };
   };
+  //use destructing to include some of the gameBoard function
   const resetBoard = () => {
-    _gameBoardInit();
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        _gameBoard[i][j] = "";
+      }
+    }
   };
 
   return {
+    gameBoardInit,
     fillBoardPosition,
     getBoardPosition,
     checkBoardEmpty,
+    isBoardBlank,
     checkBoardWin,
     resetBoard,
   };
 })();
 
-function createPlayer(name, mark) {
+function createPlayer(name, mark, score = 0) {
   const player = {};
   player.name = name;
   player.mark = mark;
+  player.score = score;
   const getPlayerName = () => player.name;
   const getPlayerMark = () => player.mark;
-  const playerReset = (name, mark) => {
-    player.name = name || "";
-    player.mark = mark || "";
+  const getPlayerScore = () => player.score;
+  const playerReset = () => {
+    player.score = 0;
   };
   const playerWon = () => {
-    console.log(`${player.name} with ${player.mark} Won!`);
+    player.score += 1;
+    alert(
+      `${player.name} with ${player.mark} won! his score is ${player.score}`
+    );
   };
 
-  return { getPlayerName, getPlayerMark, playerReset, playerWon };
+  return {
+    getPlayerName,
+    getPlayerMark,
+    getPlayerScore,
+    playerReset,
+    playerWon,
+  };
 }
 
 const displayController = (() => {
@@ -96,10 +116,21 @@ const displayController = (() => {
         const boardCell = document.createElement("div");
         boardCell.innerText = _boardArray.getBoardPosition(i, j);
         boardCell.classList.add("board-cell");
+        boardCell.setAttribute("data-x", i);
+        boardCell.setAttribute("data-y", j);
         _boardReference.appendChild(boardCell);
       }
     }
   };
+
+  const renderBoardReset = () => {
+    const boardCells = document.querySelectorAll(".gameBoard .board-cell");
+    boardCells.forEach((cell) => {
+      _boardReference.removeChild(cell);
+    });
+    renderBoard();
+  };
+
   const renderPlayers = (p1, p2) => {
     _playerProfile1.firstElementChild.innerText = p1.getPlayerName();
     _playerProfile1.lastElementChild.innerText = p1.getPlayerMark();
@@ -107,9 +138,11 @@ const displayController = (() => {
     _playerProfile2.firstElementChild.innerText = p2.getPlayerName();
     _playerProfile2.lastElementChild.innerText = p2.getPlayerMark();
   };
+  const renderNullTurn = () => {
+    _playerProfile1.children[1].classList.remove("selectedPlayer");
+    _playerProfile2.children[1].classList.remove("selectedPlayer");
+  };
   const renderSetTurn = (p) => {
-    // console.log(_playerProfile1.firstElementChild.innerText);
-    // console.log(p.getPlayerName());
     if (_playerProfile1.firstElementChild.innerText == p.getPlayerName()) {
       _playerProfile1.children[1].classList.add("selectedPlayer");
     } else {
@@ -120,23 +153,28 @@ const displayController = (() => {
     _playerProfile1.children[1].classList.toggle("selectedPlayer");
     _playerProfile2.children[1].classList.toggle("selectedPlayer");
   };
-  const renderSelectBox = (mark) => {
-    const boardCells = document.querySelectorAll(".gameBoard .board-cell");
-    boardCells.forEach((val) => {
-      val.addEventListener("click", (e) => {
-        e.target.innerText = mark;
-      });
-    });
+  const renderSelectBox = (x, y, mark) => {
+    const selectedCell = document.querySelector(
+      `[data-x="${x}"][data-y="${y}"]`
+    );
+    selectedCell.innerHTML = mark;
+  };
+  const renderScore = (s1, s2) => {
+    const score1 = document.querySelector(".score1");
+    const score2 = document.querySelector(".score2");
+    score1.innerText = s1;
+    score2.innerText = s2;
   };
 
-  //  const renderScore = (score) => {};
-  //maybe make an init function and hide away some functions
   return {
     renderBoard,
+    renderBoardReset,
     renderPlayers,
     renderSetTurn,
+    renderNullTurn,
     renderToggleTurn,
     renderSelectBox,
+    renderScore,
   };
 })();
 
@@ -146,42 +184,60 @@ const gameInstance = (() => {
   const _displayController = displayController;
   let _currentPlayerTurn;
   let _gameOver;
+  let _winningPlayer;
   let _winner;
-  let _score;
 
-  //
+  //will use this for init and reset
   function _gameInit() {
     _player1 = createPlayer("P1", "O");
     _player2 = createPlayer("P2", "X");
-    _gameBoard.resetBoard();
-    // document.addEventListener("DOMContentLoaded", function () {
+    _gameBoard.gameBoardInit();
     _displayController.renderBoard();
     _displayController.renderPlayers(_player1, _player2);
     _currentPlayerTurn = _randomPlayerTurn();
-    //_displayController.renderScore();
-    // });
+    _resetMatch();
+  }
+  //need to create clean util functions like clean states etc
+  function _resetMatch() {
+    const button = document.querySelector(".options button:last-child");
+    button.addEventListener("click", () => {
+      _gameBoard.resetBoard();
+      _displayController.renderBoardReset();
+      _gameOver = 0;
+      _displayController.renderNullTurn();
+      _currentPlayerTurn = _randomPlayerTurn();
+      _playTurn();
+      _player1.playerReset();
+      _player2.playerReset();
+      _displayController.renderScore(
+        _player1.getPlayerScore(),
+        _player2.getPlayerScore()
+      );
+    });
+  }
+  //add _gameOver when all slots filled
+  function _gameRematch() {
+    if (_gameOver) {
+      _gameBoard.resetBoard();
+      _displayController.renderBoardReset();
+      _gameOver = 0;
+      _playTurn();
+    }
   }
 
-  // const test = () => {
-  //   player tests complete
-  // };
-
-  function _gameReset() {
-    _gameBoard.resetBoard();
-    _player1.playerReset();
-    _player2.playerReset();
-    _displayController.renderBoard();
-    _currentPlayerTurn = _randomPlayerTurn();
+  function _rematch() {
+    const button = document.querySelector(".options button:first-child");
+    button.addEventListener("click", () => {
+      _gameRematch();
+    });
   }
   //can include option for manual first turn
   function _randomPlayerTurn() {
     const randomNumber = Math.random();
     if (randomNumber < 0.5) {
-      alert("random player is 1 rn");
       _displayController.renderSetTurn(_player1);
       return _player1;
     }
-    alert("random player is 2 rn");
     _displayController.renderSetTurn(_player2);
     return _player2;
   }
@@ -189,29 +245,68 @@ const gameInstance = (() => {
     _currentPlayerTurn = _currentPlayerTurn === _player1 ? _player2 : _player1;
     _displayController.renderToggleTurn();
   }
+
   function _playTurn() {
-    // document.addEventListener("DOMContentLoaded", function () {
-    // if (_currentPlayerTurn) {
-    _displayController.renderSelectBox(_currentPlayerTurn.getPlayerMark());
-    setTimeout(() => {
-      _turnToggle();
-    }, 1000);
-    // } else {
-    //   console.error("_currentPlayerTurn is not defined");
-    // }
-    // });
+    const boardCells = document.querySelectorAll(".gameBoard .board-cell");
+    boardCells.forEach((val) => {
+      val.addEventListener("click", (e) => {
+        if (e.target.innerText == "") {
+          if (_gameOver) return;
+          const x = parseInt(e.target.getAttribute("data-x"));
+          const y = parseInt(e.target.getAttribute("data-y"));
+
+          if (_gameBoard.checkBoardEmpty(x, y)) {
+            let mark = _currentPlayerTurn.getPlayerMark();
+            _displayController.renderSelectBox(x, y, mark);
+            _gameBoard.fillBoardPosition(x, y, mark);
+            e.target.classList.add("cell-clicked");
+            _checkGameStatus();
+            if (_gameOver) {
+              alert(`${_gameOver}`);
+              _setWinningPlayer(_winner);
+              _showWinnerPlayer(_winner);
+              _updateScore();
+              _rematch();
+              return;
+            }
+            _turnToggle();
+          } else {
+            // console.log(`Position (${x}, ${y}) is already occupied.`);
+          }
+        } else {
+          // console.log("Cell is already occupied.");
+        }
+      });
+    });
+  }
+
+  function _updateScore() {
+    _displayController.renderScore(
+      _player1.getPlayerScore(),
+      _player2.getPlayerScore()
+    );
+  }
+
+  function _checkGameStatus() {
+    if (!_gameBoard.isBoardBlank()) {
+      const { win, mark } = _gameBoard.checkBoardWin();
+      _gameOver = win;
+      _winner = mark;
+    }
+  }
+  function _setWinningPlayer(mark) {
+    if (_player1.getPlayerMark() == mark) {
+      _winningPlayer = _player1;
+    } else {
+      _winningPlayer = _player2;
+    }
+  }
+  function _showWinnerPlayer(mark) {
+    _winningPlayer.playerWon();
   }
   const playGame = () => {
-    //prompt for player names later
     _gameInit();
     _playTurn();
-    // setTimeout(_turnToggle(), 5000);
-    // while (!_gameOver) {
-    //   _playTurn();
-    //   result = _gameBoard.checkBoardWin();
-    //   _gameOver = result[0];
-    //   _winner = result[1];
-    // }
   };
 
   return { playGame };
